2b61af0b1a94d4a7b1d8292b1f30a7927c6df1f4
Jean Luc Houedanou <jeanluc@houedanou.com>
2026-03-30 17:51:40 +0000

    fix: retirer .nuxt et node_modules du suivi Git

--- .nuxt/ui.colors.d.ts
+++
@@ -1,1 +0,0 @@
-declare module '#ui-colors' { const defaultExport: ["red","orange","amber","yellow","lime","green","emerald","teal","cyan","sky","blue","indigo","violet","purple","fuchsia","pink","rose","fc-blue","fc-red","primary"]; export default defaultExport; }
